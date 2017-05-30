ecc1ed832eb6a668f61ee9c4c6aa1b19892be12b
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-05-30 16:58:31 +0900

    Plot multiple results

--- visualize.R
+++ visualize.R
@@ -1,15 +1,35 @@
 library(tidyverse)
-library(pipeR)
 library(wtl)
 
-.tsv = read_tsv('activities.tsv')
-.tsv
+.indirs = wtl::command_args()$args
+if (length(.indirs) < 1L) {
+    .indirs = list.dirs(full.names=FALSE, recursive=FALSE)
+}
 
-.p = .tsv %>>%
-    dplyr::mutate(copies= copies / 500) %>>%
+.infiles = file.path(.indirs, 'activities.tsv') %>%
+    purrr::keep(file.exists)
+
+extract_params = function(filename, params=c('lambda', 'xi', 'nu')) {
+    patterns = sprintf('_%s([^_]+)_', params)
+    str_match(paste0('_', filename), patterns)[,2] %>%
+    parse_double() %>%
+    set_names(params) %>%
+    as.list() %>%
+    as_tibble()
+}
+extract_params(.infiles[1])
+
+.data = .infiles %>% set_names() %>%
+    map_df(extract_params, .id='infile') %>%
+    dplyr::mutate(data= purrr::map(infile, read_tsv)) %>%
+    tidyr::unnest() %>% print()
+
+.data %>%
+    dplyr::mutate(copies= copies / 500) %>%
     ggplot(aes(time, copies, group=activity))+
     geom_area(aes(fill=activity))+
-    wtl::scale_fill_gradientb('Spectral', reverse=TRUE)+
-    wtl::theme_wtl()
-.p
-ggsave('activity.png', .p, width=10, height=7)
+    facet_grid(desc(nu) ~ desc(xi) + lambda, labeller=label_both)+
+    # wtl::scale_fill_gradientb('Spectral', reverse=TRUE)+
+    scale_fill_gradientn(colours=rev(head(rainbow(15L), 12L)), breaks=c(0, 0.5, 1))+
+    wtl::theme_wtl()+
+    theme(legend.position='bottom')
