92afe59d41215edc2e20a5acb0802284bcd56cef
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-07-05 10:50:45 +0900

    Add --skip option

--- run.py
+++ run.py
@@ -20,10 +20,12 @@ def iter_values(rest):
             yield wopt.OrderedDict(**di, **dj)
 
 
-def iter_args(rest, concurrency, repeat):
+def iter_args(rest, concurrency, repeat, skip):
     const = [program, '-j{}'.format(concurrency)] + rest
     suffix = '_{}_{}'.format(wopt.now(), wopt.getpid())
     for i, v in enumerate(wopt.cycle(iter_values(rest), repeat)):
+        if i < skip:
+            continue
         args = wopt.make_args(v)
         label = wopt.join(args) + suffix + '_{:02}'.format(i)
         yield const + args + ['--outdir=' + label]
@@ -34,11 +36,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--dry-run', action='store_true')
     parser.add_argument('-j', '--jobs', type=int, default=wopt.cpu_count())
+    parser.add_argument('--skip', type=int, default=0)
     parser.add_argument('-o', '--outdir', default='.stdout')
     parser.add_argument('-r', '--repeat', type=int, default=1)
     (args, rest) = parser.parse_known_args()
 
-    wopt.map_async(iter_args(rest, args.jobs, args.repeat),
+    wopt.map_async(iter_args(rest, args.jobs, args.repeat, args.skip),
                    1, args.dry_run, outdir=args.outdir)
     print('End of ' + __file__)
 
