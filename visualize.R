cb1ae2bcdfae52619ae53c5236004052780fcf08
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-12-31 12:05:49 +0900

    :art: Apply styler

--- visualize.R
+++ visualize.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(jsonlite)
 library(wtl)
-loadNamespace('cowplot')
+loadNamespace("cowplot")
 
 # #######1#########2#########3#########4#########5#########6#########7#########
 
-extract_params = function(filename, params=c('alpha', 'beta', 'lambda', 'xi', 'nu')) {
-    patterns = sprintf('_%s([^_]+)_', params)
-    str_match(paste0('_', filename), patterns)[,2] %>%
+extract_params = function(filename, params=c("alpha", "beta", "lambda", "xi", "nu")) {
+  patterns = sprintf("_%s([^_]+)_", params)
+  str_match(paste0("_", filename), patterns)[, 2] %>%
     parse_double() %>%
     set_names(params) %>%
     as.list() %>%
@@ -16,97 +16,98 @@ extract_params = function(filename, params=c('alpha', 'beta', 'lambda', 'xi', 'n
 # .infiles[1] %>% extract_params()
 
 count_activity = function(x) {
-    dplyr::group_by(x, generation) %>%
-    dplyr::count(activity, wt=copy_number) %>%
-    dplyr::mutate(copy_number=n / 500)
+  dplyr::group_by(x, generation) %>%
+    dplyr::count(activity, wt = copy_number) %>%
+    dplyr::mutate(copy_number = n / 500)
 }
 # .histories$data[[4]] %>% count_activity()
 
 count_per_individual = function(x) {
-    dplyr::group_by(x, generation, gamete) %>%
-    dplyr::summarise(copy_number=sum(copy_number)) %>%
+  dplyr::group_by(x, generation, gamete) %>%
+    dplyr::summarise(copy_number = sum(copy_number)) %>%
     dplyr::ungroup() %>%
-    tidyr::complete(generation, gamete, fill=list(copy_number=0L)) %>%
+    tidyr::complete(generation, gamete, fill = list(copy_number = 0L)) %>%
     dplyr::count(generation, copy_number)
 }
 # .histories$data[[4]] %>% count_per_individual()
 
 plot_copynumber_generation = function(data) {
-    ggplot(data, aes(generation, copy_number, group=activity))+
-    geom_area(aes(fill=activity))+
-    scale_fill_gradientn(colours=rev(head(rainbow(15L), 12L)), breaks=c(0, 0.5, 1))+
-    wtl::theme_wtl()+
-    theme(legend.position='bottom')
+  ggplot(data, aes(generation, copy_number, group = activity)) +
+    geom_area(aes(fill = activity)) +
+    scale_fill_gradientn(colours = rev(head(rainbow(15L), 12L)), breaks = c(0, 0.5, 1)) +
+    wtl::theme_wtl() +
+    theme(legend.position = "bottom")
 }
 # .counted %>% plot_copynumber_generation()
 
 # #######1#########2#########3#########4#########5#########6#########7#########
 
-.indirs = list.dirs(full.names=FALSE, recursive=FALSE)
+.indirs = list.dirs(full.names = FALSE, recursive = FALSE)
 .indirs = wtl::command_args()$args
-.indirs = '.'
+.indirs = "."
 
-.infiles = file.path(.indirs, 'activity.tsv.gz') %>%
-    purrr::keep(file.exists) %>%
-    print()
+.infiles = file.path(.indirs, "activity.tsv.gz") %>%
+  purrr::keep(file.exists) %>%
+  print()
 
-.histories = .infiles %>% set_names() %>%
-    map_df(extract_params, .id='infile') %>%
-    dplyr::mutate(data= purrr::map(infile, read_tsv)) %>%
-    print()
+.histories = .infiles %>%
+  set_names() %>%
+  map_df(extract_params, .id = "infile") %>%
+  dplyr::mutate(data = purrr::map(infile, read_tsv)) %>%
+  print()
 
 .counted = .histories %>%
-    dplyr::mutate(data= purrr::map(data, count_activity)) %>%
-    tidyr::unnest() %>%
-    print()
+  dplyr::mutate(data = purrr::map(data, count_activity)) %>%
+  tidyr::unnest() %>%
+  print()
 
 .nested = .counted %>%
-    tidyr::nest(-xi) %>%
-    dplyr::mutate(plt= purrr::map2(data, paste0('xi = ', xi), ~{
-        plot_copynumber_generation(.x)+
-        facet_grid(alpha + desc(nu) ~ lambda, labeller=label_both)+
-        labs(title=.y)
-    }))
+  tidyr::nest(-xi) %>%
+  dplyr::mutate(plt = purrr::map2(data, paste0("xi = ", xi), ~{
+    plot_copynumber_generation(.x) +
+      facet_grid(alpha + desc(nu) ~ lambda, labeller = label_both) +
+      labs(title = .y)
+  }))
 
-.p = cowplot::plot_grid(plotlist=.nested$plt, nrow=1)
+.p = cowplot::plot_grid(plotlist = .nested$plt, nrow = 1)
 .p
-.nested$plt[[1]] + coord_cartesian(ylim=c(0, 370))
+.nested$plt[[1]] + coord_cartesian(ylim = c(0, 370))
 
-ggsave('fig-s2.png', .p, width=15, height=12)
+ggsave("fig-s2.png", .p, width = 15, height = 12)
 
 
 .per_individual = .histories %>%
-    dplyr::mutate(data= purrr::map(data, count_per_individual)) %>%
-    unnest() %>%
-    print()
+  dplyr::mutate(data = purrr::map(data, count_per_individual)) %>%
+  unnest() %>%
+  print()
 
 .p = .per_individual %>%
-    dplyr::filter(0L <= generation, generation <= 2000L) %>%
-    ggplot(aes(copy_number, n, group=generation))+
-    geom_line(aes(colour=generation), size=1, alpha=0.8)+
-    wtl::scale_colour_gradientb('Spectral', 4L)+
-    facet_grid(alpha + desc(nu) ~ desc(xi) + lambda, labeller=label_both, scale='free_x')+
-    theme_bw()+theme(legend.position='bottom')
+  dplyr::filter(0L <= generation, generation <= 2000L) %>%
+  ggplot(aes(copy_number, n, group = generation)) +
+  geom_line(aes(colour = generation), size = 1, alpha = 0.8) +
+  wtl::scale_colour_gradientb("Spectral", 4L) +
+  facet_grid(alpha + desc(nu) ~ desc(xi) + lambda, labeller = label_both, scale = "free_x") +
+  theme_bw() + theme(legend.position = "bottom")
 .p
-ggsave('copies_per_individual.pdf', .p, width=5, height=5)
+ggsave("copies_per_individual.pdf", .p, width = 5, height = 5)
 
 # #######1#########2#########3#########4#########5#########6#########7#########
 
-.tbl_fitness = read_tsv('fitness.tsv.gz') %>% print()
+.tbl_fitness = read_tsv("fitness.tsv.gz") %>% print()
 .p = .tbl_fitness %>%
-    dplyr::filter(generation >= 400L) %>%
-    {
-      .max = . %>% group_by(generation) %>% summarise(fitness=max(fitness))
-      ggplot(., aes(fitness, group=generation, colour=generation))+
-      geom_line(aes(y=..count..), stat='bin', binwidth=0.02, size=1, alpha=0.5)+
-      geom_point(data=.max, y=-10, size=2, alpha=0.5)+
-      wtl::scale_colour_gradientb('Spectral', 4L)+
-      coord_cartesian(xlim=c(0, 1), ylim=c(0, 500))+
-      labs(title=getwd() %>% basename())+
-      theme_bw()+theme(legend.position='bottom')
-    }
+  dplyr::filter(generation >= 400L) %>%
+  {
+    .max = . %>% group_by(generation) %>% summarise(fitness = max(fitness))
+    ggplot(., aes(fitness, group = generation, colour = generation)) +
+      geom_line(aes(y = ..count..), stat = "bin", binwidth = 0.02, size = 1, alpha = 0.5) +
+      geom_point(data = .max, y = -10, size = 2, alpha = 0.5) +
+      wtl::scale_colour_gradientb("Spectral", 4L) +
+      coord_cartesian(xlim = c(0, 1), ylim = c(0, 500)) +
+      labs(title = getwd() %>% basename()) +
+      theme_bw() + theme(legend.position = "bottom")
+  }
 .p
-ggsave('fitness_history.pdf', .p, width=5, height=5)
+ggsave("fitness_history.pdf", .p, width = 5, height = 5)
 
 # #######1#########2#########3#########4#########5#########6#########7#########
 
@@ -115,77 +116,90 @@ n = 500L
 r = 5000L
 
 .fast = map_int(seq_len(r), ~{
-    v = sample(w,  2L * n, replace=TRUE)
-    v[v > max(w) * runif(2L * n)] %>% head(n) %>% table() %>% head(1)
-}) %>% {tibble(p= . / n, method='fast')}
+  v = sample(w, 2L * n, replace = TRUE)
+  v[v > max(w) * runif(2L * n)] %>% head(n) %>% table() %>% head(1)
+}) %>% {
+  tibble(p = . / n, method = "fast")
+}
 .honest = map_int(seq_len(r), ~{
-    v = sample(w, 12L * n, replace=TRUE)
-    v[v > runif(12L * n)] %>% head(n) %>% table() %>% head(1)
-}) %>% {tibble(p= . / n, method='honest')}
+  v = sample(w, 12L * n, replace = TRUE)
+  v[v > runif(12L * n)] %>% head(n) %>% table() %>% head(1)
+}) %>% {
+  tibble(p = . / n, method = "honest")
+}
 
 .p = bind_rows(.fast, .honest) %>% {
-    ggplot(., aes(p, group=method, fill=method))+
-    geom_density(alpha=0.5)+
+  ggplot(., aes(p, group = method, fill = method)) +
+    geom_density(alpha = 0.5) +
     theme_bw()
 }
 .p
-ggsave('selection_methods.pdf', .p, width=4, height=4)
+ggsave("selection_methods.pdf", .p, width = 4, height = 4)
 
 # #######1#########2#########3#########4#########5#########6#########7#########
 
-.cols = c('site', 'species', 'indel', 'nonsynonymous', 'synonymous', 'activity')
+.cols = c("site", "species", "indel", "nonsynonymous", "synonymous", "activity")
 .nsam = 100L
-.gametes = 'summary.json.gz' %>% fromJSON()
+.gametes = "summary.json.gz" %>% fromJSON()
 
-.summary = .gametes %>% sample(.nsam) %>%
-    {tibble(tmpcol=., gamete=seq_along(.))} %>%
-    tidyr::unnest() %>%
-    tidyr::separate(tmpcol, .cols, ':') %>%
-    dplyr::mutate_at(vars(c('site', 'species', 'indel', 'nonsynonymous', 'synonymous')), as.integer) %>%
-    dplyr::mutate(activity= as.double(activity)) %>%
-    dplyr::mutate(dn= nonsynonymous / 200, ds= synonymous / 100) %>%
-    dplyr::mutate(dn_ds= dn / ds, distance= (nonsynonymous + synonymous) / 300) %>%
-    print()
-
-.summary %>%
-    ggplot(aes(dn_ds, group=activity))+
-    geom_histogram(aes(fill=activity), binwidth=0.1, center=0)+
-    scale_fill_gradientn(colours=rev(head(rainbow(15L), 12L)), breaks=c(0, 0.5, 1))+
-    labs(x= 'dN/dS', y= 'Copy number')+
-    theme_bw()
+.summary = .gametes %>%
+  sample(.nsam) %>%
+  {
+    tibble(tmpcol = ., gamete = seq_along(.))
+  } %>%
+  tidyr::unnest() %>%
+  tidyr::separate(tmpcol, .cols, ":") %>%
+  dplyr::mutate_at(vars(c("site", "species", "indel", "nonsynonymous", "synonymous")), as.integer) %>%
+  dplyr::mutate(activity = as.double(activity)) %>%
+  dplyr::mutate(dn = nonsynonymous / 200, ds = synonymous / 100) %>%
+  dplyr::mutate(dn_ds = dn / ds, distance = (nonsynonymous + synonymous) / 300) %>%
+  print()
 
 .summary %>%
-    ggplot(aes(distance, dn_ds))+
-    geom_jitter(aes(colour= activity), alpha=0.3)+
-    scale_colour_gradientn(colours=rev(head(rainbow(15L), 12L)), breaks=c(0, 0.5, 1))+
-    geom_hline(aes(yintercept= 1.0), linetype='dashed')+
-    coord_cartesian(xlim=c(0, 1))+
-    labs(x= 'Distance from original sequence', y= 'dN/dS')+
-    theme_bw()
+  ggplot(aes(dn_ds, group = activity)) +
+  geom_histogram(aes(fill = activity), binwidth = 0.1, center = 0) +
+  scale_fill_gradientn(colours = rev(head(rainbow(15L), 12L)), breaks = c(0, 0.5, 1)) +
+  labs(x = "dN/dS", y = "Copy number") +
+  theme_bw()
 
 .summary %>%
-    dplyr::count(site) %>%
-    dplyr::mutate(freq = n / .nsam) %>%
-    ggplot(aes(freq))+
-    geom_histogram(binwidth=0.1, center=0)+
-    coord_cartesian(xlim=c(0, 1))+
-    labs(x= 'TE frequency', y= 'Site number')+
-    theme_bw()
+  ggplot(aes(distance, dn_ds)) +
+  geom_jitter(aes(colour = activity), alpha = 0.3) +
+  scale_colour_gradientn(colours = rev(head(rainbow(15L), 12L)), breaks = c(0, 0.5, 1)) +
+  geom_hline(aes(yintercept = 1.0), linetype = "dashed") +
+  coord_cartesian(xlim = c(0, 1)) +
+  labs(x = "Distance from original sequence", y = "dN/dS") +
+  theme_bw()
+
+.summary %>%
+  dplyr::count(site) %>%
+  dplyr::mutate(freq = n / .nsam) %>%
+  ggplot(aes(freq)) +
+  geom_histogram(binwidth = 0.1, center = 0) +
+  coord_cartesian(xlim = c(0, 1)) +
+  labs(x = "TE frequency", y = "Site number") +
+  theme_bw()
 
 # #######1#########2#########3#########4#########5#########6#########7#########
 
 library(Biostrings)
 library(ape)
 
-.fa = Biostrings::readBStringSet('sequence.fa.gz') %>%
-    {setNames(., str_extract(names(.), '^\\S+'))} %>%
-    print()
+.fa = Biostrings::readBStringSet("sequence.fa.gz") %>%
+  {
+    setNames(., str_extract(names(.), "^\\S+"))
+  } %>%
+  print()
 
-.mat = .fa %>% sample(16L) %>% stringDist('hamming')
-.cl = hclust(.mat, 'single'); plot(.cl)
-.cl = hclust(.mat, 'complete'); plot(.cl)
-.cl = hclust(.mat, 'average'); plot(.cl)
-.cl = hclust(.mat, 'mcquitty'); plot(.cl)
+.mat = .fa %>% sample(16L) %>% stringDist("hamming")
+.cl = hclust(.mat, "single")
+plot(.cl)
+.cl = hclust(.mat, "complete")
+plot(.cl)
+.cl = hclust(.mat, "average")
+plot(.cl)
+.cl = hclust(.mat, "mcquitty")
+plot(.cl)
 
 .tree = nj(.mat)
-plot(.tree, type='u')
+plot(.tree, type = "u")
