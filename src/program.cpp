0a0f81cecef6be92a5e34f5bdfa22c92311d3fd6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-10-06 01:08:41 +0900

    :art: Replace boost::program_options with clippson

--- src/program.cpp
+++ src/program.cpp
@@ -15,25 +15,19 @@
 #include <wtl/chrono.hpp>
 #include <wtl/zlib.hpp>
 #include <wtl/filesystem.hpp>
-#include <wtl/getopt.hpp>
-#include <boost/program_options.hpp>
+#include <clippson/clippson.hpp>
 
 namespace tek {
 
-namespace po = boost::program_options;
+nlohmann::json VM;
 
-//! directory for output
-const std::string OUT_DIR = wtl::strftime("tek_%Y%m%d_%H%M%S");
-
-//! options description for general arguments
-inline po::options_description general_desc() {HERE;
-    po::options_description description("General");
-    description.add_options()
-        ("help,h", po::bool_switch(), "print this help")
-        ("version", po::bool_switch(), "print version")
-        ("verbose,v", po::bool_switch(), "verbose output")
-    ;
-    return description;
+//! Options description for general purpose
+inline clipp::group general_options(nlohmann::json* vm) {HERE;
+    return (
+      wtl::option(vm, {"h", "help"}, false, "print this help"),
+      wtl::option(vm, {"version"}, false, "print version"),
+      wtl::option(vm, {"v", "verbose"}, false, "verbose output")
+    ).doc("General:");
 }
 
 /*! @ingroup params
@@ -48,19 +42,22 @@ inline po::options_description general_desc() {HERE;
     `-r,--record`       |         | Program::record_flags_
     `-o,--outdir`       |         | Program::outdir_
 */
-po::options_description Program::options_desc() {HERE;
-    auto po_value = [](auto* x) {return po::value(x)->default_value(*x);};
-    po::options_description description("Program");
-    description.add_options()
-      ("popsize,n", po_value(&popsize_))
-      ("initial,q", po_value(&initial_freq_))
-      ("generations,g", po_value(&num_generations_))
-      ("split,s", po_value(&num_generations_after_split_))
-      ("interval,i", po_value(&record_interval_))
-      ("record,r", po_value(&record_flags_))
-      ("outdir,o", po::value(&outdir_)->default_value(OUT_DIR))
-    ;
-    return description;
+inline clipp::group program_options(nlohmann::json* vm) {HERE;
+    const std::string outdir = wtl::strftime("tek_%Y%m%d_%H%M%S");
+    return (
+      wtl::option(vm, {"n", "popsize"}, 500u),
+      wtl::option(vm, {"q", "initial"}, 1u,
+        "initial number of individuals with TE"),
+      wtl::option(vm, {"g", "generations"}, 1000u,
+        "maximum number of generations to simulate"),
+      wtl::option(vm, {"s", "split"}, 0u,
+        "number of generations to simulate after population split"),
+      wtl::option(vm, {"i", "interval"}, 10u,
+        "interval of recording"),
+      wtl::option(vm, {"r", "record"}, 3,
+        "enum Recording"),
+      wtl::option(vm, {"o", "outdir"}, outdir)
+    ).doc("Program:");
 }
 
 /*! @ingroup params
@@ -71,15 +68,13 @@ po::options_description Program::options_desc() {HERE;
     `-j,--parallel`     |               | Population::CONCURRENCY_
     `-c,--coexist`      |               | Population::MAX_COEXISTENCE_
 */
-inline po::options_description Population_options(PopulationParams* p) {HERE;
-    auto po_value = [](auto* x) {return po::value(x)->default_value(*x);};
-    po::options_description description("Population");
-    description.add_options()
-      ("sample", po_value(&p->SAMPLE_SIZE))
-      ("parallel,j", po_value(&p->CONCURRENCY))
-      ("coexist,c", po_value(&p->MAX_COEXISTENCE))
-    ;
-    return description;
+inline clipp::group
+population_options(nlohmann::json* vm, PopulationParams* p) {HERE;
+    return (
+      wtl::option(vm, {"sample"}, &p->SAMPLE_SIZE),
+      wtl::option(vm, {"j", "parallel"}, &p->CONCURRENCY),
+      wtl::option(vm, {"c", "coexist"}, &p->MAX_COEXISTENCE)
+    ).doc("Population:");
 }
 
 /*! @ingroup params
@@ -90,15 +85,13 @@ inline po::options_description Population_options(PopulationParams* p) {HERE;
     `--nu`              | \f$\nu\f$     | Haploid::EXCISION_RATE_
     `--lambda`          | \f$\lambda\f$ | Haploid::MEAN_SELECTION_COEF_
 */
-inline po::options_description Haploid_options(HaploidParams* p) {HERE;
-    auto po_value = [](auto* x) {return po::value(x)->default_value(*x);};
-    po::options_description description("Haploid");
-    description.add_options()
-      ("xi", po_value(&p->XI))
-      ("nu", po_value(&p->EXCISION_RATE))
-      ("lambda", po_value(&p->MEAN_SELECTION_COEF))
-    ;
-    return description;
+inline clipp::group
+haploid_options(nlohmann::json* vm, HaploidParams* p) {HERE;
+    return (
+      wtl::option(vm, {"xi"}, &p->XI),
+      wtl::option(vm, {"nu"}, &p->EXCISION_RATE),
+      wtl::option(vm, {"lambda"}, &p->MEAN_SELECTION_COEF)
+    ).doc("Haploid:");
 }
 
 /*! @ingroup params
@@ -110,21 +103,18 @@ inline po::options_description Haploid_options(HaploidParams* p) {HERE;
     `--spec`            |               | Transposon::SPECIATION_RATE_
     `-d,--mindist`      |               | Transposon::MIN_DISTANCE_
 */
-inline po::options_description Transposon_options(TransposonParams* p) {HERE;
-    auto po_value = [](auto* x) {return po::value(x)->default_value(*x);};
-    po::options_description description("Transposon");
-    description.add_options()
-      ("alpha,a", po_value(&p->ALPHA))
-      ("beta,b", po_value(&p->BETA))
-      ("spec", po_value(&p->SPECIATION_RATE))
-      ("lower,l", po_value(&p->LOWER_THRESHOLD))
-      ("upper,u", po_value(&p->UPPER_THRESHOLD))
-    ;
-    return description;
+inline clipp::group
+transposon_options(nlohmann::json* vm, TransposonParams* p) {HERE;
+    return (
+      wtl::option(vm, {"a", "alpha"}, &p->ALPHA),
+      wtl::option(vm, {"b", "beta"}, &p->BETA),
+      wtl::option(vm, {"spec"}, &p->SPECIATION_RATE),
+      wtl::option(vm, {"l", "lower"}, &p->LOWER_THRESHOLD),
+      wtl::option(vm, {"u", "upper"}, &p->UPPER_THRESHOLD)
+    ).doc("Transposon:");
 }
 
 Program::Program(const std::vector<std::string>& arguments) {HERE;
-    wtl::join(arguments, std::cout, " ") << std::endl;
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.precision(15);
@@ -134,31 +124,33 @@ Program::Program(const std::vector<std::string>& arguments) {HERE;
     HaploidParams haploid_params;
     TransposonParams transposon_params;
 
-    auto description = general_desc();
-    description.add(options_desc());
-    description.add(Population_options(&population_params));
-    description.add(Haploid_options(&haploid_params));
-    description.add(Transposon_options(&transposon_params));
-    po::variables_map vm;
-    po::store(po::command_line_parser({arguments.begin() + 1, arguments.end()}).
-              options(description).run(), vm);
-    if (vm["help"].as<bool>()) {
+    VM.clear();
+    nlohmann::json vm_local;
+    auto cli = (
+      general_options(&vm_local),
+      program_options(&VM),
+      population_options(&VM, &population_params),
+      haploid_options(&VM, &haploid_params),
+      transposon_options(&VM, &transposon_params)
+    );
+    wtl::parse(cli, arguments);
+    auto fmt = wtl::doc_format();
+    if (vm_local["help"]) {
         std::cout << "Usage: " << PROJECT_NAME << " [options]\n\n";
-        description.print(std::cout);
+        std::cout << clipp::documentation(cli, fmt) << "\n";
         throw wtl::ExitSuccess();
     }
-    if (vm["version"].as<bool>()) {
+    if (vm_local["version"]) {
         std::cout << PROJECT_VERSION << "\n";
         throw wtl::ExitSuccess();
     }
-    po::notify(vm);
     Population::param(population_params);
     Haploid::param(haploid_params);
     Transposon::param(transposon_params);
-    config_string_ = wtl::flags_into_string(vm);
-    if (vm["verbose"].as<bool>()) {
+    config_ = VM.dump(2);
+    if (vm_local["verbose"]) {
         std::cerr << wtl::iso8601datetime() << std::endl;
-        std::cerr << config_string_ << std::endl;
+        std::cerr << config_ << std::endl;
     }
 }
 
@@ -171,13 +163,20 @@ void Program::run() {HERE;
 }
 
 void Program::main() {HERE;
+    const size_t popsize_ = VM.at("popsize");
+    const size_t initial_freq_ = VM.at("initial");
+    const size_t num_generations_ = VM.at("generations");
+    const size_t num_generations_after_split_ = VM.at("split");
+    const size_t record_interval_ = VM.at("interval");
+    const int record_flags_ = VM.at("record");
+    const std::string outdir_ = VM.at("outdir");
     wtl::ChDir cd_outdir(outdir_, true);
     while (true) {
         Population pop(popsize_, initial_freq_);
         auto flags = static_cast<Recording>(record_flags_);
         bool good = pop.evolve(num_generations_, record_interval_, flags);
         if (!good) continue;
-        wtl::make_ofs("program_options.conf") << config_string_;
+        wtl::make_ofs("config.json") << config_;
         if (static_cast<bool>(flags & Recording::sequence)) {
             wtl::zlib::ofstream ost("sequence.fa.gz");
             pop.write_fasta(ost);
--- src/program.hpp
+++ src/program.hpp
@@ -8,8 +8,6 @@
 #include <vector>
 #include <string>
 
-namespace boost {namespace program_options {class options_description;}}
-
 namespace tek {
 
 /*! @brief Program class
@@ -20,7 +18,7 @@ class Program {
     Program(const std::vector<std::string>& args);
     //! Parse command arguments
     Program(int argc, char* argv[])
-    : Program(std::vector<std::string>(argv, argv + argc)) {}
+    : Program(std::vector<std::string>(argv + 1, argv + argc)) {}
 
     //! Top level function that should be called once from global main
     void run();
@@ -28,32 +26,9 @@ class Program {
   private:
     //! called from run()
     void main();
-    //! options description for Program class
-    boost::program_options::options_description options_desc();
 
-    /////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
-    //! @addtogroup params
-    //! @{
-
-    //! \f$N\f$, population size
-    size_t popsize_ = 500u;
-    //! @} params
-    /////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
-
-    //! initial number of individuals with TE
-    size_t initial_freq_ = 1u;
-    //! maximum number of generations to simulate
-    size_t num_generations_ = 1000u;
-    //! number of generations to simulate after population split
-    size_t num_generations_after_split_ = 0u;
-    //! interval of recording
-    size_t record_interval_ = 10u;
-    //! enum Recording
-    int record_flags_ = 3;
-    //! name of output directory
-    std::string outdir_;
-    //! writen to "program_options.conf"
-    std::string config_string_;
+    //! writen to "config.json"
+    std::string config_;
 };
 
 } // namespace tek
