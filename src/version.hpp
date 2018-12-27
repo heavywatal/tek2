b0eec531e74238d8a6ae0e305d62a4666d89fc69
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-12-28 01:04:24 +0900

    :art: Configure .cpp.in to avoid superfluous compilation

---
+++ src/version.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef TEK_VERSION_HPP
+#define TEK_VERSION_HPP
+
+namespace tek {
+
+extern const char* const PROJECT_NAME;
+extern const char* const PROJECT_VERSION;
+
+}
+
+#endif // TEK_VERSION_HPP
