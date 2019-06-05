fb2d6e6d23cd4957ca11eb04283013e1d81ec120
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-06-05 23:19:55 +0900

    :art: Refactor fitness.R

--- rstats/fitness.R
+++ rstats/fitness.R
@@ -1,17 +1,17 @@
 library(tidyverse)
 
 .tbl_fitness = read_tsv("fitness.tsv.gz") %>% print()
-.p = .tbl_fitness %>%
-  dplyr::filter(generation >= 400L) %>%
-  {
-    .max = . %>% group_by(generation) %>% summarise(fitness = max(fitness))
-    ggplot(., aes(fitness, group = generation, colour = generation)) +
-      geom_line(aes(y = ..count..), stat = "bin", binwidth = 0.02, size = 1, alpha = 0.5) +
-      geom_point(data = .max, y = -10, size = 2, alpha = 0.5) +
-      wtl::scale_colour_gradientb("Spectral", 4L) +
-      coord_cartesian(xlim = c(0, 1), ylim = c(0, 500)) +
-      labs(title = getwd() %>% basename()) +
-      theme_bw() + theme(legend.position = "bottom")
-  }
+
+N = max(dplyr::count(.tbl_fitness, generation)$n)
+df = .tbl_fitness %>% dplyr::filter(generation >= 400L)
+df_max = .max = df %>% dplyr::group_by(generation) %>% summarise(fitness = max(fitness))
+
+.p = ggplot(df, aes(fitness, group = generation, colour = generation)) +
+  geom_line(aes(y = stat(count)), stat = "bin", binwidth = 0.02, size = 1, alpha = 0.5) +
+  geom_point(data = df_max, y = -10, size = 2, alpha = 0.5) +
+  scale_colour_distiller(palette = "Spectral", direction = 1) +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, N)) +
+  labs(title = basename(getwd())) +
+  theme_bw() + theme(legend.position = "bottom")
 .p
 ggsave("fitness_history.pdf", .p, width = 5, height = 5)
