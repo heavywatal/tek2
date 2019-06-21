14fa646725482ce22c9513debee23f4cdb3d91f2
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-06-21 11:04:45 +0900

    :construction: Try single bit sequence

--- src/dna.hpp
+++ src/dna.hpp
@@ -12,10 +12,26 @@
 #include <numeric>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
 namespace tek {
+
+template <size_t N> inline
+std::bitset<2 * N> make_odd_bits() {
+    std::ostringstream oss;
+    for (size_t i=0; i<N; ++i) {
+        oss << "01";
+    }
+    return std::bitset<2 * N>(oss.str());
+}
+
+template <size_t N> inline
+std::bitset<N> odd_and(const std::bitset<N>& x) {
+    static const std::bitset<N> mask = make_odd_bits<N / 2>();
+    return mask & x;
+}
 
 /*! @brief DNA class
 */
@@ -31,14 +47,13 @@ class DNA {
     //! construct from sequence
     DNA(std::valarray<uint_fast8_t>&& s) noexcept {
         for (unsigned i=0; i<N; ++i) {
-            has_3bonds_.set(i, 0b10u & s[i]);
-            is_pyrimidine_.set(i, 0b01u & s[i]);
+            bits_ |= s[N - i];
+            bits_ <<= 2u;
+            // bits_.set(i + N, 0b10u & s[i]);
+            // bits_.set(i, 0b01u & s[i]);
+            // has_3bonds_.set(i, 0b10u & s[i]);
+            // is_pyrimidine_.set(i, 0b01u & s[i]);
         }
-    }
-
-    //! diviation from the original
-    uint_fast32_t count() const noexcept {
-        return (is_pyrimidine_ | has_3bonds_).count();
     }
 
     //! mutate i-th site
@@ -47,10 +62,15 @@ class DNA {
         typename URBG::result_type random_bits = 0u;
         while ((random_bits = engine()) == 0u) {;}
         while ((0b11u & random_bits) == 0u) {
-            random_bits >>= 2;
+            random_bits >>= 2u;
         }
-        if (0b10u & random_bits) {has_3bonds_.flip(i);}
-        if (0b01u & random_bits) {is_pyrimidine_.flip(i);}
+        std::bitset<2 * N> mask(0b00000011u & random_bits);
+        mask <<= (2u * i);
+        bits_ ^= mask;
+        // if (0b10u & random_bits) {bits_.flip(i + N);}
+        // if (0b01u & random_bits) {bits_.flip(i)};
+        // if (0b10u & random_bits) {has_3bonds_.flip(i);}
+        // if (0b01u & random_bits) {is_pyrimidine_.flip(i);}
     }
 
     //! get i-th nucleotide
@@ -60,7 +80,10 @@ class DNA {
 
     //! get i-th nucleotide as integer
     uint_fast8_t get(uint_fast32_t i) const noexcept {
-        return (has_3bonds_[i] << 1) + is_pyrimidine_[i];
+        static const std::bitset<2 * N> mask(0b00000011u);
+        return ((bits_ >> (2u * i)) & mask).to_ulong();
+        // return (bits_[i + N] << 1) + bits_[i];
+        // return (has_3bonds_[i] << 1) + is_pyrimidine_[i];
     }
 
     //! translate integer to nucleotide character and print
@@ -76,9 +99,19 @@ class DNA {
         return x.write(ost);
     }
 
+    //! diviation from the original
+    uint_fast32_t count() const noexcept {
+        return (odd_and(bits_) | odd_and(bits_ >> 1u)).count();
+        // return ((bits_ >> N) | (bits_ << N >> N)).count();
+        // return (is_pyrimidine_ | has_3bonds_).count();
+    }
+
     //! Hamming distance
     uint_fast32_t operator-(const DNA& other) const noexcept {
-        return ((this->is_pyrimidine_ ^ other.is_pyrimidine_) | (this->has_3bonds_ ^ other.has_3bonds_)).count();
+        auto diff = bits_ ^ other.bits_;
+        return (odd_and(diff) | odd_and(diff >> 1u)).count();
+        // return ((diff >> N) | (diff << N >> N)).count();
+        // return ((this->is_pyrimidine_ ^ other.is_pyrimidine_) | (this->has_3bonds_ ^ other.has_3bonds_)).count();
     }
 
   private:
@@ -95,11 +128,8 @@ class DNA {
           });
     }
 
-    // sequence as two bitsets: {00: A, 01: T, 10: G, 11: C}
-    //! {0: AT, 1: GC}
-    std::bitset<N> has_3bonds_;
-    //! {0: AG, 1: TC}
-    std::bitset<N> is_pyrimidine_;
+    //! sequence as two bitsets: {00: A, 01: T, 10: G, 11: C}
+    std::bitset<2 * N> bits_;
 };
 
 /*! @brief Homolog class
